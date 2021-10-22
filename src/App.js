b32b0c36261756805c5218265c6a79945d59e9a9
Daniel Vital <49732779+JorgeDanielVital@users.noreply.github.com>
2021-10-22 11:10:08 -0500

    get data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Table, Button, Container } from 'reactstrap'
 
-function App() {
+const App = () => {
+  const baseUrl = "http://127.0.0.1:5000/";
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    await axios.get(baseUrl)
+      .then(res => {
+        setData(res.data)
+      }).catch(error => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Container>
+        <br />
+        <Button color="primary">Insertar nueva Data</Button>
+        <br />
+        <Table>
+          <thead>
+            <tr>
+              <th>
+                level_1
+              </th>
+              <th>
+                level_2
+              </th>
+              <th>
+                year
+              </th>
+              <th>
+                value
+              </th>
+              <th>
+                acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              data.map(element => (
+                <tr>
+                  <td>
+                    {element.level_1}
+                  </td>
+                  <td>
+                    {element.level_2}
+                  </td>
+                  <td>
+                    {element.year}
+                  </td>
+                  <td>
+                    {element.value}
+                  </td>
+                  <td>
+                    <Button color="primary">
+                      Editar
+                    </Button>
+                  </td>
+                  <td>
+                    <Button color="danger">
+                      Eliminar
+                    </Button>
+                  </td>
+                </tr>)
+              )}
+          </tbody>
+        </Table>
+
+      </ Container>
+    </>
+
+  )
 }
+
 
 export default App;
